925e2e3aadd85022a283ea0c068e03f31920a005
GabrielLopes001 <gabrieljob1@outlook.com>
2024-04-02 23:20:05 -0300

    style: add solid icon when focusing on menu

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -5,6 +5,11 @@ import {
   HomeIcon,
   UserCircleIcon,
 } from 'react-native-heroicons/outline'
+import {
+  HeartIcon as HeartIconSolid,
+  HomeIcon as HomeIconSolid,
+  UserCircleIcon as UserCircleIconSolid,
+} from 'react-native-heroicons/solid'
 
 export default function TabsLayout() {
   return (
@@ -24,25 +29,34 @@ export default function TabsLayout() {
         <Tabs.Screen
           name="index"
           options={{
-            tabBarIcon: ({ size, color }) => (
-              <HomeIcon size={size} color={color} />
-            ),
+            tabBarIcon: ({ size, color, focused }) =>
+              focused ? (
+                <HomeIconSolid size={size} color={color} />
+              ) : (
+                <HomeIcon size={size} color={color} />
+              ),
           }}
         />
         <Tabs.Screen
           name="favorite"
           options={{
-            tabBarIcon: ({ size, color }) => (
-              <HeartIcon size={size} color={color} />
-            ),
+            tabBarIcon: ({ size, color, focused }) =>
+              focused ? (
+                <HeartIconSolid size={size} color={color} />
+              ) : (
+                <HeartIcon size={size} color={color} />
+              ),
           }}
         />
         <Tabs.Screen
           name="profile"
           options={{
-            tabBarIcon: ({ size, color }) => (
-              <UserCircleIcon size={size} color={color} />
-            ),
+            tabBarIcon: ({ size, color, focused }) =>
+              focused ? (
+                <UserCircleIconSolid size={size} color={color} />
+              ) : (
+                <UserCircleIcon size={size} color={color} />
+              ),
           }}
         />
       </Tabs>
